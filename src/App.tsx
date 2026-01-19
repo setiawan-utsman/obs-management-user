421f04b97be433cc788c1512d5081e21533b5fdb
unknown <yayuk@jkt1.ebdesk.com>
2026-01-19 10:51:14 +0700

    update development

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,8 @@ function App() {
         {/* Left */}
         <div className="flex items-center gap-3">
           <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-primary/10">
-            <Users className="h-5 w-5 text-primary" />
+            {/* <Users className="h-5 w-5 text-primary" /> */}
+            <img src="./logo.png" alt="" />
           </div>
 
           <div className="leading-tight">
